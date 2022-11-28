d5ac4b3c9998abadfa87d4675e8755c630565445
mnishimori <mnishimori@yahoo.com.br>
2022-11-28 18:28:20 -0300

    feature: configuração e recebimento de mensagens do RabbitMq

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,5 @@
-FROM openjdk:17
+FROM openjdk:17-alpine
+#RUN addgroup -S spring && adduser -S spring -G spring
+#USER spring:spring
 COPY target/pedidos*SNAPSHOT.jar pedidos.jar
 ENTRYPOINT ["java", "-jar", "/pedidos.jar"]
--- src/main/java/br/com/alurafood/pedidos/PedidosApplication.java
+++ src/main/java/br/com/alurafood/pedidos/PedidosApplication.java
@@ -2,9 +2,8 @@ package br.com.alurafood.pedidos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
-@EnableEurekaClient
+//@EnableEurekaClient
 @SpringBootApplication
 public class PedidosApplication {
 
